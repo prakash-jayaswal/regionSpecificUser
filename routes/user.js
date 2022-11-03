2aa8b9f12076aabf68d0913cb54678f1ec6c1564
prakashjayaswal <prakashjayaswal62@gmail.com>
2022-11-03 22:24:58 +0530

    code optimized as per timing

--- routes/user.js
+++ routes/user.js
@@ -6,17 +6,23 @@ export default function (Router ) {
     Router.post('/signup', async (req, res) => {
         // console.log('args', req.body);
         try {
+            let date0 = new Date()
             if (!req.body.email) throw new Error('Email is mandatory')
             // check for correct message
+            // console.log('route step 1',new Date()-date0);
             let check = await User.exists({ email: req.body.email })
             if (check) throw new Error('Email already exist')
+            // console.log('route step 2',new Date()-date0);
             let {regionName,location} = await getRegion(req.body.lat, req.body.lng)
             if (!regionName) throw new Error('Not belongs to indian regions')
+            // console.log('route step 3',new Date()-date0);
             req.body.location = location
             req.body.region = regionName
             // lets register in DB
             let user = new User(req.body)
+            // console.log('route step 4',new Date()-date0);
             user = await user.save()
+            // console.log('route step 5',new Date()-date0);
             return res.status(200).json(user)
         } catch (e) {
             return res.status(500).send(e.message)
--- utils/region.js
+++ utils/region.js
@@ -2,13 +2,17 @@ import data from "../Indian_State_2020.json" assert { type: "json" };
 import PolygonLookup from 'polygon-lookup'
 
 export const getRegion = async (lat, lng) => {
-    // console.log('START getRegion');
+     let date = new Date()
+   // console.log('START getRegion');
     if (!lat || !lng) return {}
+    // console.log('start0 getRegion',new Date()-date);
     var lookup = new PolygonLookup(data);
     var poly = lookup.search(lng,lat );
     // console.log(poly);
     if (poly) {
+    // console.log('start1 getRegion',new Date()-date);
     let regionName = checkInRegion(poly)
+    // console.log('start2 getRegion',new Date()-date);
     let location = poly?.properties?.NAME_1
     if(regionName) return {regionName,location}
     }
@@ -17,71 +21,43 @@ export const getRegion = async (lat, lng) => {
 
 
 const checkInRegion = (polygon) => {
+    let date = new Date()
     let featuresIds = []
     for (let feature of data.features)
         featuresIds.push(feature.id)
-    let regionsArray = sliceIntoChunks(featuresIds, 8)
-    
+    // console.log('start1 sliceIntoRegions',new Date()-date);
+    let regionsArray = sliceIntoRegions(featuresIds, 8)
+    // console.log('start2 sliceIntoRegions',new Date()-date);
     for (let r of regionsArray)
         if(r.includes(polygon.id)) return `region-${regionsArray.indexOf(r)}`;
 }
 
-const sliceIntoChunks =(arr, chunkSize)=> {
+const sliceIntoRegions = (arr, regionSize) => {
+    let date = new Date()
+    // console.log('before sorting',new Date()-date);
+    let sortedArray = bblSort(arr)
+    // console.log('after sorting', new Date() - date);
     const res = [];
-    for (let i = 0; i < arr.length; i += chunkSize) {
-        const chunk = arr.slice(i, i + chunkSize);
+    for (let i = 0; i < sortedArray.length; i += regionSize) {
+        const chunk = sortedArray.slice(i, i + regionSize);
         res.push(chunk);
     }
+    // console.log('after split', new Date() - date);
+    // for (let i of res) console.log('i',i.length);
     return res;
 }
 
 
-
-
-// NOT worked for all test cases
-// const isPointInsidePolygon = (region, lat, lng) => {
-//     // console.log('START isPointInsidePolygon',region.geometry);
-//     for (let coordinate of region.geometry.coordinates) {
-//         // for (let polygon of coordinate) {
-//                 let ans = classifyPoint(coordinate, [lat, lng])
-//                 if(ans == -1 || ans == 0) return true
-            
-//             // if (pointInPolygon([lat, lng], polygon))
-//                 // return true
-            
-//             //  if (insidePoly(coordinate,lat, lng))
-//             //     return true
-            
-//             // console.log('End polygon');
-//         // }
-//     }
-// }
-
-// // function inside(point, vs) {
-// //     console.log('pp',point, vs);
-// //     // ray-casting algorithm based on
-// //     // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
-    
-// //     var x = point[0], y = point[1];
-    
-// //     var inside = false;
-// //     for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-// //         var xi = vs[i][0], yi = vs[i][1];
-// //         var xj = vs[j][0], yj = vs[j][1];
-        
-// //         var intersect = ((yi > y) != (yj > y))
-// //             && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-// //         if (intersect) inside = !inside;
-// //     }
-    
-// //     return inside;
-// // };
-
-// function insidePoly(poly, pointx, pointy) {
-//     var i, j;
-//     var inside = false;
-//     for (i = 0, j = poly.length - 1; i < poly.length; j = i++) {
-//         if(((poly[i].y > pointy) != (poly[j].y > pointy)) && (pointx < (poly[j].x-poly[i].x) * (pointy-poly[i].y) / (poly[j].y-poly[i].y) + poly[i].x) ) inside = !inside;
-//     }
-//     return inside;
-// }
+// Creating the bblSort function
+const bblSort = (arr)=>{
+ for(var i = 0; i < arr.length; i++){    
+   for(var j = 0; j < ( arr.length - i -1 ); j++){
+     if(arr[j] > arr[j+1]){     
+       var temp = arr[j]
+       arr[j] = arr[j + 1]
+       arr[j+1] = temp
+     }
+   }
+ }
+return arr
+}
